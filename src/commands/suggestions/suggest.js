dc186d4dac5cbbaa4289c863f5691a6ec8a8e47c
izexi7 <jhannan644@gmail.com>
2024-05-19 02:38:40 +0100

    update suggest command

--- src/commands/suggestions/suggest.js
+++ src/commands/suggestions/suggest.js
@@ -1,8 +1,10 @@
 const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 const generateId = require('../../utils/generateId');
 const SuggestionChannel = require('../../schemas/suggestionSchema');
+const config = require('../../../config');
 
 module.exports = {
+    cooldown: 10,
     data: new SlashCommandBuilder()
         .setName('suggest')
         .setDescription('Submit a suggestion')
@@ -11,37 +13,54 @@ module.exports = {
                 .setDescription('Content of suggestion')
                 .setRequired(true)),
     async execute(interaction) {
-        const suggestion = interaction.options.getString('suggestion');
-        const suggestionChannelData = await SuggestionChannel.findOne({ guildId: interaction.guild.id });
-
-        if (!suggestionChannelData) {
-            return interaction.reply({ content: 'Suggestion channel has not been set up.', ephemeral: true });
-        }
-
-        const suggestionChannel = interaction.client.channels.cache.get(suggestionChannelData.channelId);
-        if (!suggestionChannel) {
-            return interaction.reply({ content: 'Suggestion channel not found.', ephemeral: true });
-        }
-
-        const suggestionId = generateId();
-        const suggestionEmbed = new EmbedBuilder()
-            .setDescription(suggestion)
-            .setThumbnail(interaction.user.displayAvatarURL())
-            .setAuthor({ name: interaction.user.tag, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
-            .setFooter({ text: `Suggestion ID: ${suggestionId} | Submitted at` })
-            .setTimestamp();
-
         try {
-            await suggestionChannel.send({ embeds: [suggestionEmbed] });
-            const confirmationEmbed = new EmbedBuilder()
-                .setColor(0x00ff00)
-                .setDescription(`Your suggestion has been submitted! (ID: ${suggestionId})`);
+            const suggestion = interaction.options.getString('suggestion');
+            const suggestionChannelData = await SuggestionChannel.findOne({ guildId: interaction.guild.id });
 
-            await interaction.reply({ embeds: [confirmationEmbed], ephemeral: true });
+            if (!suggestionChannelData) {
+                return interaction.reply({ content: 'Suggestion channel has not been set up.', ephemeral: true });
+            }
+
+            const suggestionChannel = interaction.client.channels.cache.get(suggestionChannelData.channelId);
+            if (!suggestionChannel) {
+                return interaction.reply({ content: 'Suggestion channel not found.', ephemeral: true });
+            }
+
+            let rolesToMention = '';
+            if (suggestionChannelData.roles && suggestionChannelData.roles.length > 0) {
+                rolesToMention = suggestionChannelData.roles.map(roleId => `<@&${roleId}>`).join(' ');
+            }
+
+            const suggestionId = generateId();
+            const suggestionEmbed = new EmbedBuilder()
+                .setColor(config.HexColour)
+                .setDescription(suggestion)
+                .setThumbnail(interaction.user.displayAvatarURL())
+                .setAuthor({ name: `Suggestion from ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
+                .setFooter({ text: `Suggestion ID: ${suggestionId} | Submitted at` })
+                .addFields(
+                    { name: 'Votes', value: `Upvotes: \`0\`\nDownVotes: \`0\`` },
+                )     
+                .setTimestamp();
+                
+            const suggestionMessage = await suggestionChannel.send({ content: rolesToMention, embeds: [suggestionEmbed] });
+
+            await suggestionMessage.react('👍');
+            await suggestionMessage.react('🤷‍♂️');
+            await suggestionMessage.react('👎');
+
+            const confirmationEmbed = new EmbedBuilder()
+                .setColor(config.HexColour)
+                .setAuthor({ name: `Suggestion from ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL({ dynamic: true }) })
+                .setDescription(suggestion)
+                .setFooter({ text: `Suggestion ID: ${suggestionId} | Submitted at` })
+                .setTimestamp();
+
+            await interaction.reply({ content: 'Your suggestion has been submitted to the server staff for review!', embeds: [confirmationEmbed] });
         } catch (error) {
-            console.error('Error sending suggestion:', error);
+            console.error('[SUGGESTION]', error);
             const errorEmbed = new EmbedBuilder()
-                .setColor(0xff0000)
+                .setColor(config.HexColour)
                 .setDescription('There was an error submitting your suggestion. Please try again later.');
 
             await interaction.reply({ embeds: [errorEmbed], ephemeral: true });
