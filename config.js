f68b0347aa816a0b7062a0d3ed1715fa782b9423
izexi7 <jhannan644@gmail.com>
2024-05-19 02:38:16 +0100

    update config

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
     token: 'MTI0MTU0MDI1NzE2MjIwMzE1OA.GwwTF0.FqG1Rm8QYdjZkAhqiZV7oGXBs1I3Z7r0LqPVkk',
     id: '1241540257162203158',
+    status: 'Use /help to get started.',
     devguild: '1230177085960618034',
     developer: '1161617296402423898',
     errorWebhookUrl: 'https://discord.com/api/webhooks/1237797300626264094/0fT2hUFGyQVPvGA76KcYRbw7kb5obY16qqGnWJU8uMbaFjjgUzsmQcMLNhgVrcluORa0',
